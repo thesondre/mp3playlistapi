978301e0e3372b2c8aa2644e81b49163275173bb
thesondre <160282070+thesondre@users.noreply.github.com>
2026-01-02 20:38:28 +0100

    Created api for configuration

--- main.py
+++ main.py
@@ -2,10 +2,42 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+config = {
+    "mp3Dir":"~/Music",
+    "playlistDir":"~/Music/Playlists",
+    "relativePath":"../"
+}
 @app.get("/")
 async def read_root():
     return {"message": "Hello, FastAPI!"}
 
+##Config
+#############################################################################################
+@app.get("/config")
+def get_config():
+    return config
+
+@app.patch("/config")
+def change_config(mp3Dir:str, playlistDir:str, relativePath:str):
+    
+    if(mp3Dir):
+        config["mp3Dir"] = mp3Dir
+    if(playlistDir):
+        config["playlistDir"] = playlistDir
+    if(relativePath):
+        config["relativePath"] = relativePath
+    return config
+
+@app.post("/config/reset")          #Default setting
+def reset_config():
+   global config 
+   config = {                               
+        "mp3Dir":"~/Music",
+        "playlistDir":"~/Music/Playlists",
+        "relativePath":"../"
+    }
+   return "Config resetted"
+#############################################################################################
 def main():
     pass
 
